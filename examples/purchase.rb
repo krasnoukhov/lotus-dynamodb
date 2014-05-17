f80982ac625ccc7d946e212186593099558b3b96
Dmitry Krasnoukhov <dmitry@krasnoukhov.com>
2014-05-17 04:07:02 +0300

    Fix example

--- examples/purchase.rb
+++ examples/purchase.rb
@@ -14,60 +14,60 @@ AWS.config(
 
 DB = AWS::DynamoDB::Client.new(api_version: Lotus::Dynamodb::API_VERSION)
 
-# begin
-#   # DB.describe_table("purchases")
-# rescue AWS::DynamoDB::Errors::ResourceNotFoundException
-#   DB.create_table(
-#     table_name: "purchases",
-#
-#     # List of all attributes which are used as table and indexes keys
-#     attribute_definitions: [
-#       { attribute_name: "region",       attribute_type: "S" },
-#       { attribute_name: "created_at",   attribute_type: "N" },
-#       { attribute_name: "subtotal",     attribute_type: "N" },
-#       { attribute_name: "uuid",         attribute_type: "S" },
-#     ],
-#
-#     # Key schema of table
-#     key_schema: [
-#       { attribute_name: "region",       key_type: "HASH" },
-#       { attribute_name: "created_at",   key_type: "RANGE" },
-#     ],
-#
-#     # List of local indexes
-#     local_secondary_indexes: [{
-#       index_name: "by_subtotal",
-#       key_schema: [
-#         { attribute_name: "region",     key_type: "HASH" },
-#         { attribute_name: "subtotal",   key_type: "RANGE" },
-#       ],
-#       projection: {
-#         projection_type: "ALL",
-#       },
-#     }],
-#
-#     # List of global indexes
-#     global_secondary_indexes: [{
-#       index_name: "by_uuid",
-#       key_schema: [
-#         { attribute_name: "uuid",       key_type: "HASH" },
-#       ],
-#       projection: {
-#         projection_type: "ALL",
-#       },
-#       provisioned_throughput: {
-#         read_capacity_units: 10,
-#         write_capacity_units: 10,
-#       },
-#     }],
-#
-#     # Capacity
-#     provisioned_throughput: {
-#       read_capacity_units: 10,
-#       write_capacity_units: 10,
-#     },
-#   )
-# end
+begin
+  DB.describe_table("purchases")
+rescue AWS::DynamoDB::Errors::ResourceNotFoundException
+  DB.create_table(
+    table_name: "purchases",
+
+    # List of all attributes which are used as table and indexes keys
+    attribute_definitions: [
+      { attribute_name: "region",       attribute_type: "S" },
+      { attribute_name: "created_at",   attribute_type: "N" },
+      { attribute_name: "subtotal",     attribute_type: "N" },
+      { attribute_name: "uuid",         attribute_type: "S" },
+    ],
+
+    # Key schema of table
+    key_schema: [
+      { attribute_name: "region",       key_type: "HASH" },
+      { attribute_name: "created_at",   key_type: "RANGE" },
+    ],
+
+    # List of local indexes
+    local_secondary_indexes: [{
+      index_name: "by_subtotal",
+      key_schema: [
+        { attribute_name: "region",     key_type: "HASH" },
+        { attribute_name: "subtotal",   key_type: "RANGE" },
+      ],
+      projection: {
+        projection_type: "ALL",
+      },
+    }],
+
+    # List of global indexes
+    global_secondary_indexes: [{
+      index_name: "by_uuid",
+      key_schema: [
+        { attribute_name: "uuid",       key_type: "HASH" },
+      ],
+      projection: {
+        projection_type: "ALL",
+      },
+      provisioned_throughput: {
+        read_capacity_units: 10,
+        write_capacity_units: 10,
+      },
+    }],
+
+    # Capacity
+    provisioned_throughput: {
+      read_capacity_units: 10,
+      write_capacity_units: 10,
+    },
+  )
+end
 
 #
 # Define
